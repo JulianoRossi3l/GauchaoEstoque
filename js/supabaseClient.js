47f4842e0994e83e20cfd0784e75c77055a3b923
Juliano Rossi <julianorossi3l@gmail.com>
2026-03-16 07:08:34 -0400

    Refactor Supabase client initialization

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -2,6 +2,5 @@
 const SUPABASE_URL = 'https://yfhnszjcuhmypgcgyxdi.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlmaG5zempjdWhteXBnY2d5eGRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM2MzkzNzUsImV4cCI6MjA4OTIxNTM3NX0.X-wA2UplJpBI-C1DWI0LW_u9BI7CmM1AZ5ekwV9bYaU';
 
-const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
-
-export { _supabase as supabase };
+// Usamos window.supabase para garantir que pegamos a biblioteca do script do HTML
+export const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
